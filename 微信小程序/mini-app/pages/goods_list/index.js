edeb0771990738a94e054b7731c6139c9df07ee6
lily <943165920@qq.com>
2019-09-24 19:14:55 +0800

    商品列表实现数据渲染

--- 微信小程序/mini-app/pages/goods_list/index.js
+++ 微信小程序/mini-app/pages/goods_list/index.js
@@ -1,4 +1,11 @@
-// pages/goods_list/index.js
+/**
+ * 总页数 = Math.ceil(总条数 / 页容量)
+ */
+
+
+
+import {request} from '../../request/index.js'
+
 Page({
 
   //页面的初始数据
@@ -9,8 +16,21 @@ Page({
       {id : 1, text : "销量"},
       {id : 2, text : "价格"}
     ],
-    currentIndex : 0  //当前的索引
+    currentIndex : 0,  //当前的索引
+    goodsList : [],  //商品列表数据
+    isShow : false
   },
+
+  //使用全局变量存储发送请求的参数
+  goodsObj : {
+    query : '',
+    cid : '',
+    pagenum : 1,
+    pagesize : 10
+  },
+
+  //定义总页数
+  totalPages : 1,
 
   //处理切换tablan
   handSendIndex(e) {
@@ -18,6 +38,60 @@ Page({
     this.setData({
       currentIndex : e.detail.index
     })
+  },
+
+  onLoad(options) {
+    // console.log(options)
+    this.goodsObj.cid = options.cid
+
+    this.getGoodsList()
+  },
+
+  //获取商品列表数据
+  getGoodsList() {
+    request({url : '/goods/search',data : this.goodsObj})
+    .then(res => {
+      console.log(res)
+      //需要实现新旧数据结合
+      const newGoodsList = res.data.message.goods
+      const oldGoodsList = this.data.goodsList
+
+      const {total} = res.data.message
+      this.totalPages = Math.ceil(total / this.goodsObj.pagesize)
+
+      this.setData({
+        goodsList : [...oldGoodsList,...newGoodsList]
+      })
+
+      //数据回来了,关闭下拉动作
+      wx.stopPullDownRefresh()
+    })
+  },
+
+  //页面上拉触发的事件
+  onReachBottom() {
+    //先判断有没有下一项数据
+    if(this.goodsObj.pagenum >= this.totalPages) {
+      wx.showToast({
+        title : '最后一页了,没有数据了',
+        icon : 'none'
+      })
+      this.setData({
+        isShow : true
+      })
+    }else {
+      //让当前页数自增
+      this.goodsObj.pagenum++
+      this.getGoodsList()
+    }
+  },
+
+  //监听用户下拉动作
+  onPullDownRefresh() {
+    //重新刷新数据
+    this.goodsObj.pagenum = 1
+    this.data.goodsList = []
+    this.getGoodsList()
   }
 
 })
