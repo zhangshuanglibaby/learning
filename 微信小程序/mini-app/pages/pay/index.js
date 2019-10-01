22fa48afe23de24abe9cb9fa0f18ee3a3788eee1
lily <943165920@qq.com>
2019-10-01 23:39:30 +0800

    支付页面-页面动态渲染

--- 微信小程序/mini-app/pages/pay/index.js
+++ 微信小程序/mini-app/pages/pay/index.js
@@ -1,66 +1,57 @@
-// pages/pay/index.js
+/**
+ * 1 在页面数据渲染的时候,要加载的购物车数据应该是 checked属性 = true
+ */
+
+
+
+
+import regeneratorRuntime from '../../lib/runtime/runtime'
+import { getSetting,openSetting,chooseAddress,showModal,showToast } from '../../request/index'
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data : {
+    adress : {},  //存储地址数据
+    carts : [],  //购物车数据
+    totalPrice : 0,  //总价格
+    totalNums : 0  //结算数量
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  //监听页面显示
+  onShow() {
+    //获取缓存中的收获信息
+    //此时的adress可能是有对象,可能是空字符串 ,空字符串的布尔是false
+    const adress = wx.getStorageSync('adress')
 
+    //获取购物车数据
+    let carts = wx.getStorageSync('carts') || [];
+
+    //赋值前先过滤找出checked = true
+     carts = carts.filter(v => v.checked) 
+      
+    //赋值给data
+    this.setData({adress,carts})
+
+    //调用计算方法
+    this.coundData(carts)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  //计算数量
+  coundData(carts) {
+    //总价格是计算checked为true的商品 = 商品价钱 * 商品数量
+    let totalPrice = 0
+    //结算数量是结算checke为true的商品
+    let totalNums = 0
 
-  },
+    carts.forEach(v => {
+      if(v.checked) {
+        totalPrice += v.goods_price * v.num
+        totalNums += v.num
+      }
+    })
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    //重新赋值
+    this.setData({totalPrice,totalNums})
   }
+
 })
