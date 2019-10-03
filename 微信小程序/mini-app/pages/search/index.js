3c9089014901c579e69a50f4d4eedecc605f9b69
lily <943165920@qq.com>
2019-10-04 02:40:43 +0800

    搜索中心-页面功能完成

--- 微信小程序/mini-app/pages/search/index.js
+++ 微信小程序/mini-app/pages/search/index.js
@@ -6,6 +6,13 @@
  *   3 把返回的数据渲染到页面
  * 
  * 2 在用户输入1s后再发送请求  防抖 防止抖动 （通过定时器来清除上一次的输入）
+ * 
+ * 3 点击取消按钮
+ *   1 隐藏按钮
+ *   2 清除 数组数据
+ *   3 清除 输入框的值
+ * 
+ * 4 点击超链接 跳转到详情页面
  */
 
 import regeneratorRuntime from '../../lib/runtime/runtime'
@@ -14,7 +21,9 @@ import { request } from '../../request/index'
 Page({
 
   data: {
-    searchList: []
+    searchList: [],
+    isShow : false, // 控制按钮显示
+    inputVal : ''  //控制输入框内容
   },
   timeId: -1,
   handleInput(e) {
@@ -22,6 +31,7 @@ Page({
 
     //非空检测
     if(value.trim()) {
+      this.setData({isShow : true})
        //清除上一次的定时器
     clearTimeout(this.timeId)
     this.timeId = setTimeout(() => {
@@ -38,5 +48,15 @@ Page({
     this.setData({
       searchList: res
     })
+  },
+
+  //点击取消触发
+  handleCancel() {
+    this.setData({
+      isShow :false,
+      searchList : [],
+      inputVal : ''
+    })
+
   }
 })
