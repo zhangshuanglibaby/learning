a0456e3d78acfbe8e3be9c3274ef845faa7e032f
lily <943165920@qq.com>
2019-10-04 02:18:08 +0800

    搜索中心-防止抖动

--- 微信小程序/mini-app/pages/search/index.js
+++ 微信小程序/mini-app/pages/search/index.js
@@ -1,66 +1,42 @@
-// pages/search/index.js
+/**
+ * 1 给输入框绑定 input事件
+ *   1 获取输入框的值
+ *     1 需要做非空检测
+ *   2 把输入框的值发送给后台
+ *   3 把返回的数据渲染到页面
+ * 
+ * 2 在用户输入1s后再发送请求  防抖 防止抖动 （通过定时器来清除上一次的输入）
+ */
+
+import regeneratorRuntime from '../../lib/runtime/runtime'
+import { request } from '../../request/index'
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
+    searchList: []
+  },
+  timeId: -1,
+  handleInput(e) {
+    const { value } = e.detail
+
+    //非空检测
+    if(value.trim()) {
+       //清除上一次的定时器
+    clearTimeout(this.timeId)
+    this.timeId = setTimeout(() => {
+      this.getSearchList(value)
+    }, 1000)
+    }
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //获取搜索内容
+  async getSearchList(value) {
+    const res = await request({url: '/goods/qsearch', data: { query: value}})
+    console.log(res)
+    this.setData({
+      searchList: res
+    })
   }
 })
